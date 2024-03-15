f795f3b092441af40ca054399b539301c48a895a
Brian <snackbrian10@gmail.com>
2024-03-14 22:53:41 -0300

    Primeiro commit

---
+++ Ex06B/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exe06B;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        int[] vetor = new int[100];
+        Random aleatorio = new Random();
+
+        // Preencher vetor com valores aleatórios
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = aleatorio.Next(100) + 1;
+        }
+
+        // Imprimir vetor
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+        // Ordenação com alguma funcionalidade da linguagem
+        Array.Sort(vetor);
+        Console.WriteLine("\n");
+        // Imprimir vetor ordenado 
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            Console.Write(vetor[i] + " ");
+        }
+    }
+}
